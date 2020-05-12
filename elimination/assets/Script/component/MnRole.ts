8dbe098252f37f69839ce09def2c1ab29de7cd95
ethan <1589664970@qq.com>
2020-05-12 18:46:17 +0800

    update: framework body:     update afterimage effect     add blur effect     add discolor effect     add manual for effect programming

--- elimination/assets/Script/component/MnRole.ts
+++ elimination/assets/Script/component/MnRole.ts
@@ -1,6 +1,6 @@
 /**
  * 游戏角色
- * 将该脚本挂载到场景Canvas下
+ * 需将该脚本挂载到场景Canvas下
  */
 
 const { ccclass, property } = cc._decorator;
@@ -8,41 +8,75 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class MnRole extends cc.Component {
 
-    @property([cc.Sprite])
-    ghostCanvasList: cc.Sprite[] = []; // 残影
-
     @property(cc.Node)
     role: cc.Node = null; // 主角
 
     @property(cc.Camera)
-    roleCamera: cc.Camera = null; // 跟随主角移动，需要单独分组
+    roleCamera: cc.Camera = null; // 主角摄像机，跟随主角移动，需要单独分组
+
+    @property([cc.Sprite])
+    ghostCanvasList: cc.Sprite[] = []; // 残影
+
+    private _ghostEffect: boolean = null; // 是否开启残影
+    private _ghostTexture: cc.RenderTexture = null; // 摄像机渲染目标
 
     onLoad() {
     }
 
-    // 残影效果
+    // 增加残影效果
     public addGhostEffect(): void {
+        if (this._ghostEffect) return;
+
+        this.role.group = "role";
+
         const roleZindex = 10; // 管理主角和残影的层级
         this.role.zIndex = roleZindex;
 
         // 对场景进行截图
-        const texture = new cc.RenderTexture();
-        texture.initWithSize(this.node.width, this.node.height);
+        this._ghostTexture = new cc.RenderTexture();
+        this._ghostTexture.initWithSize(this.node.width, this.node.height);
 
         // 摄像机渲染截图
         const spriteFrame = new cc.SpriteFrame();
-        spriteFrame.setTexture(texture);
-        this.roleCamera.targetTexture = texture;
+        spriteFrame.setTexture(this._ghostTexture);
+        this.roleCamera.targetTexture = this._ghostTexture;
 
         this.ghostCanvasList.forEach((ghost, idx) => {
             ghost.node.scaleY = -1;
             ghost.node.zIndex = roleZindex - idx;
             ghost.node.opacity = 100 - idx * 15;
             ghost.spriteFrame = spriteFrame;
+            ghost.node.active = true;
         });
 
         this.schedule(this.ghostFollow, 0.1, cc.macro.REPEAT_FOREVER);
         this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMoveEvent, this);
+
+        this._ghostEffect = true;
+    }
+
+    // 移除残影效果
+    public removeGhostEffect(): void {
+        if (!this._ghostEffect) return;
+
+        this.node.off(cc.Node.EventType.TOUCH_MOVE, this.touchMoveEvent, this);
+
+        this._ghostTexture.destroy();
+        this.roleCamera.targetTexture = null;
+
+        this._ghostEffect = false;
+
+        this.ghostFollow && this.unschedule(this.ghostFollow);
+
+        this.ghostCanvasList.forEach(ghost => {
+            ghost.node.active = false;
+            ghost.node.stopAllActions();
+            ghost.spriteFrame.destroy();
+        });
+
+        let out = this.role.convertToWorldSpaceAR(cc.v2(this.role.x, this.role.y));
+        this.role.group = "default";
+        this.role.setPosition(this.role.convertToNodeSpaceAR(out));
     }
 
     touchMoveEvent(evt: cc.Event.EventTouch) {
@@ -52,6 +86,7 @@ export default class MnRole extends cc.Component {
 
     onDestroy() {
         this.ghostFollow && this.unschedule(this.ghostFollow);
+        this.node.off(cc.Node.EventType.TOUCH_MOVE, this.touchMoveEvent, this);
     }
 
     ghostFollow() {
